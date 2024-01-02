9bacddc0be101f3aef5dc097930462e566212348
georapbox <georapbox@gmail.com>
2024-01-02 12:32:48 +0200

    Create build

--- docs/index.e89179bb.js
+++
@@ -1,3 +0,0 @@
-"serviceWorker"in navigator&&navigator.serviceWorker.register("service-worker.js").catch(r=>{console.error(r)});//# sourceMappingURL=index.e89179bb.js.map
-
-//# sourceMappingURL=index.e89179bb.js.map
