f65411bafe670fcb55ffa2a09e6fc28588f27d5b
georapbox <georapbox@gmail.com>
2024-02-16 09:17:12 +0200

    Updates

---
+++ index.98a7b582.js
@@ -0,0 +1,2 @@
+"serviceWorker"in navigator&&navigator.serviceWorker.register("service-worker.js").catch(r=>{console.error(r)});
+//# sourceMappingURL=index.98a7b582.js.map
---
+++ index.bb1ce697.js
@@ -0,0 +1,2 @@
+"serviceWorker"in navigator&&navigator.serviceWorker.register("service-worker.js").catch(r=>{console.error(r)});
+//# sourceMappingURL=index.bb1ce697.js.map
