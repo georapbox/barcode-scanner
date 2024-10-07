33bb6ff44caf2432be85bc302e376c837800d776
George Raptis <georapbox@gmail.com>
2024-10-07 19:30:47 +0300

    Add effects when barcode is not detected when uploading file

--- src/js/helpers/triggerScanEffects.js
+++ src/js/helpers/triggerScanEffects.js
@@ -5,13 +5,17 @@ import { vibrate } from '../utils/vibrate.js';
 /**
  * Triggers the scan effects like beep and vibrate.
  */
-export async function triggerScanEffects() {
+export async function triggerScanEffects({ success = true } = {}) {
   const [, settings] = await getSettings();
 
   if (!settings) {
     return;
   }
 
-  settings.beep && beep({ duration: 200, frequency: 860, volume: 0.03, type: 'square' });
-  settings.vibrate && vibrate(100);
+  const beepConfig = success
+    ? { duration: 200, frequency: 860, volume: 0.03, type: 'square' }
+    : { duration: 300, frequency: 100, volume: 0.04, type: 'sawtooth' };
+
+  settings.beep && beep(beepConfig);
+  settings.vibrate && vibrate(success ? 100 : 300);
 }
